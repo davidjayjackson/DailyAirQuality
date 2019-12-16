1488dbb9f22c2e66fa77a5e74a73fa3fd0d6027b
davidjayjackson <davidjayjackson@gmail.com>
2019-12-16 17:32:20 -0500

    basic ts data cleaning. Added a couple of plots.

--- arima._forecast.R
+++ arima._forecast.R
@@ -1,3 +1,11 @@
+## Title: ARMIA Forcasting 
+## Subject: Air Quality Index
+## YouTube: Tech Know How
+## Author: David Jackson (davidjayjackson@gmail.com)
+## Date: 2019-12-16
+
+
+
 library(tidyverse)
 library(forecast)
 library(tseries)
@@ -7,13 +15,47 @@ library(data.table)
 ##
 ## Delete old data sets
 rm(list=ls())
-## Load sample data
+## Load sample data and chang date field to "date" type
 data1 <- fread("daily_aqi/daily_aqi_by_county_2010.csv")
 data1$Date <- as.Date(data1$Date)
+data1$Month <- lubridate::month(data1$Date)
+##
+data2 <- fread("daily_aqi/daily_aqi_by_county_2011.csv")
+data2$Date <- as.Date(data2$Date)
+data2$Month <- lubridate::month(data2$Date)
+##
+##
 aqi <- data1 %>% filter(`State Name`=="Maine" & `county Name`=="Penobscot")
 aqi <- as.data.table(aqi)
-ggplot(aqi,aes(x=Date,y=AQI)) + geom_point(color="navyblue") + 
-  facet_wrap(~Month) + scale_x_date("month") + 
+##
+aqi2 <- data2 %>% filter(`State Name`=="Maine" & `county Name`=="Penobscot")
+aqi2 <- as.data.table(aqi2)
+AQI <- rbind(data1,data2)
+##
+## Plot AQI over time: data1: 2010
+##
+ggplot(aqi,aes(x=Date,y=AQI)) + geom_line() + scale_x_date("month") + 
   ylab("Air Quality Index") + ggtitle(" Daily Air Qualitity Index: 2010")
-  
+##
 
+## Plot AQI over time: data2: 2011
+##
+ggplot(aqi2,aes(x=Date,y=AQI)) + geom_line() + scale_x_date("month") + 
+  ylab("Air Quality Index") + ggtitle(" Daily Air Qualitity Index: 2011")
+##
+## Plot month over month to see the range and outerliers: 2010 - 2011
+##
+AQI <- AQI %>% filter(`State Name`=="Maine" & `county Name`=="Penobscot")
+
+ggplot(AQI,aes(x=Date,y=AQI)) + geom_point(color="navyblue") + 
+  facet_wrap(~Month) + scale_x_date("month") + 
+  ylab("Air Quality Index") + ggtitle(" Combined Daily Air Qualitity Index: 2010 - 2011")
+##
+## Create time series object: 2010 - 2011
+##  tsClean function also
+AQI.ts <- ts(AQI[,c("AQI")])
+AQI$Count <- tsclean(AQI.ts)
+##
+## Plot data
+ggplot(data=AQI) + geom_point(aes(x=Date,y=Count,col="Clean",size=1)) + ggtitle("First Pass Cleaning[tsclean]") +
+ylab("Clean Counts") + geom_point(data=AQI,aes(x=Date,y=AQI,col="AQI"))
